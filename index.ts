29d08ed3b0ff817a5b87355d5dc451f03a889487
Damien Lajarretie <dlajarretie@airintservices.com>
2022-05-10 15:56:11 +0200

    initial clapping implementation

--- index.ts
+++ index.ts
@@ -1,12 +1,25 @@
-import express, { NextFunction, Request, Response } from 'express';
-import { readFileSync, existsSync, writeFileSync } from 'fs';
-import { Participant, Rump, assertIsString } from './src/utils';
-import { json } from 'body-parser';
+import express, { NextFunction, Request, Response } from "express";
+import { readFileSync, existsSync, writeFileSync } from "fs";
+import { Participant, Rump, assertIsString } from "./src/utils";
+import { json } from "body-parser";
+import { createServer } from "http";
+import { Server, Socket } from "socket.io";
 // eslint-disable-next-line node/no-unpublished-import
-const participants = JSON.parse(readFileSync('./participants.json', 'utf-8'));
+const participants = JSON.parse(readFileSync("./participants.json", "utf-8"));
 const app = express();
+const server = createServer(app);
+const io = new Server(server);
+let clapping: Set<string> = new Set();
 
-const AUTH_HEADER = 'rump-sthack-auth';
+const AUTH_HEADER = "rump-sthack-auth";
+const CLAPPING_TTL = 1000;
+
+function authenticate(qrCode: string | undefined) {
+  if (qrCode) {
+    return participants[qrCode];
+  }
+  return undefined;
+}
 
 function authenticationMiddleware(
   req: Request,
@@ -14,19 +27,15 @@ function authenticationMiddleware(
   next: NextFunction
 ) {
   const rumpSthackAuthHeader = req.header(AUTH_HEADER);
-  if (rumpSthackAuthHeader) {
-    console.log(rumpSthackAuthHeader);
-    const participant: Participant | undefined =
-      participants[rumpSthackAuthHeader];
-    if (participant) {
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      req.participant = participant;
-      return next();
-    }
+  const participant = authenticate(rumpSthackAuthHeader);
+  if (participant) {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    req.participant = participant;
+    return next();
   }
   res.status(401);
-  throw Error('Invalid authentication');
+  throw Error("Invalid authentication");
 }
 
 function initConfig() {
@@ -34,28 +43,28 @@ function initConfig() {
     port: process.env.STHACK_RUMP_PORT
       ? parseInt(process.env.STHACK_RUMP_PORT, 10)
       : 3000,
-    hostname: process.env.STHACK_RUMP_HOSTNAME ?? 'localhost',
+    hostname: process.env.STHACK_RUMP_HOSTNAME ?? "localhost",
   };
 }
 const config = initConfig();
 
-app.use(express.static('app'));
+app.use(express.static("app"));
 
 app.use(authenticationMiddleware);
 app.use(json());
 
-app.get('/rump', (req, res) => {
+app.get("/rump", (req, res) => {
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
   const participant = req.participant;
   const rumpFile = `rumps/${participant.qrCode}.json`;
   let rump: Rump = {
-    title: '',
-    description: '',
-    speaker: '',
+    title: "",
+    description: "",
+    speaker: "",
   };
   if (existsSync(rumpFile)) {
-    rump = JSON.parse(readFileSync(rumpFile, 'utf-8'));
+    rump = JSON.parse(readFileSync(rumpFile, "utf-8"));
   }
 
   assertIsString(rump.description);
@@ -64,22 +73,68 @@ app.get('/rump', (req, res) => {
   res.send(rump);
 });
 
-app.put('/rump', (req, res) => {
+app.put("/rump", (req, res) => {
   console.log(req.body);
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
   const participant = req.participant;
   const rumpFile = `rumps/${participant.qrCode}.json`;
   const rump: Rump = {
-    title: req.body.title ?? '',
-    description: req.body.description ?? '',
-    speaker: req.body.speaker ?? '',
+    title: req.body.title ?? "",
+    description: req.body.description ?? "",
+    speaker: req.body.speaker ?? "",
   };
 
-  writeFileSync(rumpFile, JSON.stringify(rump), 'utf-8');
+  writeFileSync(rumpFile, JSON.stringify(rump), "utf-8");
   res.send(rump);
 });
 
 app.listen(config.port, config.hostname, () => {
   console.log(`Server listening on ${config.hostname}:${config.port}`);
+});
+
+interface ISocket extends Socket {
+  authenticatedParticipant?: Participant | null;
+}
+
+io.on("connection", (socket: ISocket) => {
+  console.log("a user connected");
+  socket.on("auth", (qrCode: string) => {
+    console.log(`auth with QR code : ${qrCode}`);
+    const participant = authenticate(qrCode);
+    if (authenticate(qrCode)) {
+      socket.authenticatedParticipant = participant;
+    } else {
+      socket.authenticatedParticipant = null;
+    }
+  });
+  socket.on("clap", () => {
+    const participant = socket.authenticatedParticipant;
+    console.log(
+      `clap event received (authenticated user : ${participant?.displayName})`
+    );
+    if (!participant) {
+      console.log(`bad auth, ignored`);
+      return;
+    }
+    const { qrCode } = participant;
+    if (!clapping.has(qrCode)) {
+      console.log("👏");
+      clapping.add(qrCode);
+      console.log(`clapmeter : ${clapping.size}`);
+      setTimeout(() => {
+        clapping.delete(qrCode);
+        console.log(`clapmeter : ${clapping.size}`);
+      }, CLAPPING_TTL);
+    } else {
+      console.log(`clapping too fast`);
+    }
+  });
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+server.listen(3000, () => {
+  console.log("listening on *:3000");
 });
